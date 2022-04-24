99177ef8a24ac9cf44144fef7ba145ee0efeb325
Lior Kashti <lkashti@gmail.com>
2022-04-24 18:53:05 +0300

    dropped async, implemented concrete Mapping and Ec2InstanceProxy

--- src/instance-mapping/ec2_instance_mapping.py
+++ src/instance-mapping/ec2_instance_mapping.py
@@ -1,24 +1,43 @@
 from typing import Iterator
 
+from boto3 import Session  
+
 from ec2_instance_proxy import Ec2InstanceProxy, Ec2RemoteShellProxy
 from vm_instance_mapping import VmInstanceMappingBase
 from vm_instance_proxy import VmInstanceProxy
 
 
 class Ec2InstanceMapping(VmInstanceMappingBase[VmInstanceProxy]):
-    def __getitem__(self, name) -> VmInstanceProxy:
-        return self.get_instance(name)
+    def __init__(self) -> None:
+        self._client = Session().client("ec2")
+
+    def __getitem__(self, name: str) -> VmInstanceProxy:
+        instance_id = self._get_instance_id(name)
+        return self._get_instance(instance_id)
 
     def __iter__(self) -> Iterator:
-        ...  # TODO implement
+        yield from self._client.describe_instances()["Reservations"]
 
-    def __len__(str) -> int:
-        ...  # TODO implement
+    def __len__(self) -> int:
+        return sum(1 for _ in self)
 
-    def get_instance(self, name) -> Ec2InstanceProxy:
-        return Ec2InstanceProxy(name)
+    def _get_instance(self, id: str) -> Ec2InstanceProxy:
+        return Ec2InstanceProxy(id)
+
+    def _get_instance_id(self, instance_name: str) -> str:
+        instance_details = self._client.describe_instances(
+            Filters=[
+                {
+                    'Name': 'tag:Name',  # as long as you are following the convention of putting Name in tags
+                    'Values': [
+                        instance_name,
+                    ]
+                },
+            ],
+        )
+        return instance_details["Reservations"][0]["Instances"][0]["InstanceId"]
 
 
 class Ec2RemoteShellMapping(Ec2InstanceMapping, VmInstanceMappingBase):
-    def get_instance(self, name) -> Ec2RemoteShellProxy:
-        return Ec2RemoteShellProxy(name)
+    def _get_instance(self, id: str) -> Ec2RemoteShellProxy:
+        return Ec2RemoteShellProxy(id)
--- src/instance-mapping/ec2_instance_proxy.py
+++ src/instance-mapping/ec2_instance_proxy.py
@@ -1,41 +1,96 @@
-from enum import auto
-from typing import Tuple
+
+import time
+from typing import Any, Iterable, Tuple, Union
+
+from boto3 import Session, resource
+from pyclvm._common.session import get_session #TODO source out of clvm
 
 from vm_instance_proxy import VmState
 
 
-class _Ec2State(VmState):
-    UNKNOWN = auto()  # this is the only value not taken from AWS instance states
-    PENDING = auto()
-    RUNNING = auto()
-    STOPPING = auto()
-    STOPPED = auto()
-    SHUTTING_DOWN = auto()
-    TERMINATED = auto()
+class _Ec2StateProxy(VmState):
+    pending = 0
+    running = 16
+    shutting_down = 32
+    terminated = 48
+    stopping = 64
+    stopped = 80
 
 
 class Ec2InstanceProxy:
-    def __init__(self, name) -> None:
-        self._name = name
-        self._state = _Ec2State.UNKNOWN
+    def __init__(self, instance_id: str) -> None:
+        self._instance_id = instance_id
+        self._ec2_client = Session().client('ec2')
+        self._instance = resource('ec2').Instance(instance_id)
 
-    async def start(self, trace: bool) -> None:
-        ...  # TODO implement
+    def start(self, wait: bool = True) -> None:
+        """
+        Start the vm
 
-    async def stop(self, trace: bool) -> None:
-        ...  # TODO implement
+        :return: None
+        """
+        self._ec2_client.start_instances(
+            InstanceIds=[
+                self._instance_id
+            ],
+        )
+        wait and self._instance.wait_until_running()  # python short circuiting
 
-    async def reboot(self, trace: bool) -> None:
-        ...  # TODO implement
+    def stop(self, wait: bool = True) -> None:
+        """
+        Stop the vm
+
+        :return: None
+        """
+        self._ec2_client.stop_instances(
+            InstanceIds=[
+                self._instance_id
+            ],
+        )
+        wait and self._instance.wait_until_stopped()
 
     @property
-    def state(self) -> _Ec2State:  # TODO use the enum
-        return self._state
+    def state(self) -> _Ec2StateProxy:
+        return _Ec2StateProxy[self._instance.state["Name"]]
 
 
 class Ec2RemoteShellProxy(Ec2InstanceProxy):
-    def __init__(self, name) -> None:
-        ...  # TODO implement
+    def __init__(self, instance_id: str) -> None:
+        super().__init__(instance_id)
+        self._ssm_client = get_session({}).client('ssm')
 
-    async def execute(self, *commands: str) -> Tuple[str, str]:
-        ...  # TODO implement
+    def execute(
+            self,
+            *commands: Union[str, Iterable],
+            **kwargs: str
+    ) -> Union[Tuple[Any, ...], Tuple[str, str]]:
+        result = self._ssm_client.send_command(
+            InstanceIds=[self._instance_id],
+            DocumentName="AWS-RunShellScript",
+            Parameters={"commands": ["source /etc/bashrc", *commands]},
+        )
+
+        command_id = result["Command"]["CommandId"]
+        # see https://stackoverflow.com/questions/50067035/retrieving-command-invocation-in-aws-ssm
+        time.sleep(2)
+        if not kwargs.get("wait", True):
+            return self._ssm_client, command_id, self._instance_id
+        waiter = self._ssm_client.get_waiter("command_executed")
+        try:
+            waiter.wait(
+                CommandId=command_id,
+                InstanceId=self._instance_id,
+                WaiterConfig={
+                    "Delay": kwargs.get("delay", 5),
+                    "MaxAttempts": kwargs.get("attempts", 20),
+                },
+            )
+        finally:
+            result = self._ssm_client.get_command_invocation(
+                CommandId=command_id,
+                InstanceId=self._instance_id,
+                PluginName="aws:RunShellScript",
+            )
+            print(result.StandardOutputContent)
+            print(result.StandardErrorContent)
+        return result.StandardOutputContent, result.StandardErrorContent
