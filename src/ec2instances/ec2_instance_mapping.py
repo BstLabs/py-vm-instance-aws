6b682ff3e6629597ede20302f2e75fa5cbbf6197
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-05-30 17:48:27 +0400

    Release PR v1.0.2 (#20)
    * created initial skeleton for the mappings and proxies
    
    * dropped async, implemented concrete Mapping and Ec2InstanceProxy
    
    * renamed repository
    
    * removed base classes, imports are now from installed package
    
    * using pip installed vm-instance instead of package local one, created session part
    
    * changed package name
    
    * started flit
    
    * fixed relative imports
    
    * Prepared for release (#6)
    
    * Added github workflows, requirements, formatting and linting scripts and configurations, wrote README file.
    
    * Reconfigured pyproject.toml, applied corresponding naming to README file
    
    * Fixed pytype issue on CI. Fixed dependency issues
    
    * Fixed CI errors
    
    * fixed mapping iterator, added more properties
    
    * added kwargs to allow profile selection
    
    * further fixes, exposed common for import (#12)
    
    * further fixes, exposed common for import
    
    * added __init__.py file to common dir
    
    * Fixed lint error on CI
    
    Co-authored-by: orkhanshirin <orkhan.shirinow@gmail.com>
    
    * Fixed style errors in CI
    
    * Updated version
    
    * removed session, it should now be passed as an argument to the mappings
    
    * Introduced Ec2AllInstancesMapping class for clvm instance ls command: performance boost achieved (#19)
    
    * Added flag for ec2_client to be passed as argument
    
    * Introducing new class for mapping all instances in one shot; great positive performance impact
    
    * Passing the linter fail
    
    * Failed dict type for Python 3.8
    
    * Again failed linter check
    
    * Removed redundant prints
    
    * Renamed the class; removed redundant inheritance
    
    * Renamed the class; removed redundant inheritance
    
    * Failed linter
    
    * Passing linter
    
    * version bump
    
    Co-authored-by: Lior Kashti <lkashti@gmail.com>
    Co-authored-by: Lior Kashti <48313697+lkashti@users.noreply.github.com>
    Co-authored-by: Orkhan Shirin <62020621+orkhanshirin@users.noreply.github.com>
    Co-authored-by: orkhanshirin <orkhan.shirinow@gmail.com>

--- src/ec2instances/ec2_instance_mapping.py
+++ src/ec2instances/ec2_instance_mapping.py
@@ -1,15 +1,38 @@
-from typing import Generator, Iterator
+from typing import Any, Dict, Generator, Iterator, Tuple
 
+from boto3 import resource
 from instances_map_abc.vm_instance_mapping import VmInstanceMappingBase
 from instances_map_abc.vm_instance_proxy import VmInstanceProxy
 
-from .common.session import get_session
 from .ec2_instance_proxy import Ec2InstanceProxy, Ec2RemoteShellProxy
 
 
-class Ec2InstanceMapping(VmInstanceMappingBase[VmInstanceProxy]):
+class Ec2AllInstancesData:
     def __init__(self, **kwargs: str) -> None:
-        self._client = get_session(kwargs).client("ec2")
+        self._all_instances = resource("ec2").instances.all()
+        self._instances_data = [
+            (
+                _instance.id,
+                self._get_instance_name(_instance),
+                _instance.state.Code,
+                _instance.state.Name,
+            )
+            for _instance in self._all_instances
+        ]
+
+    def __iter__(self) -> Iterator:
+        yield from self._instances_data
+
+    def _get_instance_name(self, _instance: Dict[str, Any]) -> str:
+        for tag in _instance.tags:
+            if tag["Key"] == "Name":
+                return tag["Value"]
+
+
+class Ec2InstanceMapping(VmInstanceMappingBase[VmInstanceProxy]):
+    def __init__(self, session) -> None:
+        self._session = session
+        self._client = self._session.client("ec2")
 
     def __getitem__(self, name: str) -> VmInstanceProxy:
         instance_id = self._get_instance_id(name)
@@ -32,11 +55,11 @@ class Ec2InstanceMapping(VmInstanceMappingBase[VmInstanceProxy]):
     def values(self) -> Generator[str, None, None]:
         yield from self
 
-    def items(self) -> Generator[str, None, None]:
+    def items(self) -> Generator[Tuple[str, str], None, None]:
         yield from zip(self.keys(), self.values())
 
     def _get_instance(self, instance_id: str) -> Ec2InstanceProxy:
-        return Ec2InstanceProxy(instance_id)
+        return Ec2InstanceProxy(instance_id, self._session, ec2_client=self._client)
 
     def _get_instance_id(self, instance_name: str) -> str:
         instance_details = self._client.describe_instances(
@@ -54,4 +77,4 @@ class Ec2InstanceMapping(VmInstanceMappingBase[VmInstanceProxy]):
 
 class Ec2RemoteShellMapping(Ec2InstanceMapping, VmInstanceMappingBase):
     def _get_instance(self, instance_id: str) -> Ec2RemoteShellProxy:
-        return Ec2RemoteShellProxy(instance_id)
+        return Ec2RemoteShellProxy(instance_id, self._session)
--- src/ec2instances/ec2_instance_proxy.py
+++ src/ec2instances/ec2_instance_proxy.py
@@ -1,10 +1,9 @@
 import time
-from typing import Any, Iterable, Tuple, Union
+from typing import Any, Iterable, Optional, Tuple, Union
 
+import botocore
 from boto3 import resource
 from instances_map_abc.vm_instance_proxy import VmState
-
-from .common.session import get_session  # TODO source out of clvm
 
 
 class _Ec2StateProxy(VmState):
@@ -17,9 +16,15 @@ class _Ec2StateProxy(VmState):
 
 
 class Ec2InstanceProxy:
-    def __init__(self, instance_id: str, **kwargs: str) -> None:
+    def __init__(
+        self,
+        instance_id: str,
+        session,
+        ec2_client: Optional[botocore.client.BaseClient] = None,
+        **kwargs: str,
+    ) -> None:
         self._instance_id = instance_id
-        self._ec2_client = get_session(kwargs).client("ec2")
+        self._ec2_client = ec2_client or session.client("ec2")
         self._instance = resource("ec2").Instance(instance_id)
 
     def start(self, wait: bool = True) -> None:
@@ -60,9 +65,9 @@ class Ec2InstanceProxy:
 
 
 class Ec2RemoteShellProxy(Ec2InstanceProxy):
-    def __init__(self, instance_id: str, **kwargs: str) -> None:
-        super().__init__(instance_id)
-        self._session = get_session(kwargs)
+    def __init__(self, instance_id: str, session) -> None:
+        super().__init__(instance_id, session)
+        self._session = session
         self._ssm_client = self._session.client("ssm")
 
     def execute(
