5843050232d79b17df2aa3a2adcd6c7a93570270
Orkhan Shirin <62020621+orkhanshirin@users.noreply.github.com>
2022-05-31 18:06:55 +0400

    Version bump v1.0.3 (#22)
    * created initial skeleton for the mappings and proxies
    
    * dropped async, implemented concrete Mapping and Ec2InstanceProxy
    
    * renamed repository
    
    * removed base classes, imports are now from installed package
    
    * using pip installed vm-instance instead of package local one, created session part
    
    * changed package name
    
    * started flit
    
    * fixed relative imports
    
    * Prepared for release (#6)
    
    * Added github workflows, requirements, formatting and linting scripts and configurations, wrote README file.
    
    * Reconfigured pyproject.toml, applied corresponding naming to README file
    
    * Fixed pytype issue on CI. Fixed dependency issues
    
    * Fixed CI errors
    
    * fixed mapping iterator, added more properties
    
    * added kwargs to allow profile selection
    
    * further fixes, exposed common for import (#12)
    
    * further fixes, exposed common for import
    
    * added __init__.py file to common dir
    
    * Fixed lint error on CI
    
    Co-authored-by: orkhanshirin <orkhan.shirinow@gmail.com>
    
    * Fixed style errors in CI
    
    * Updated version
    
    * removed session, it should now be passed as an argument to the mappings
    
    * Introduced Ec2AllInstancesMapping class for clvm instance ls command: performance boost achieved (#19)
    
    * Added flag for ec2_client to be passed as argument
    
    * Introducing new class for mapping all instances in one shot; great positive performance impact
    
    * Passing the linter fail
    
    * Failed dict type for Python 3.8
    
    * Again failed linter check
    
    * Removed redundant prints
    
    * Renamed the class; removed redundant inheritance
    
    * Renamed the class; removed redundant inheritance
    
    * Failed linter
    
    * Passing linter
    
    * version bump
    
    * Removed redundant **kwargs from Ec2AllInstancesData __init__
    
    * Version bump
    
    Co-authored-by: Lior Kashti <lkashti@gmail.com>
    Co-authored-by: Lior Kashti <48313697+lkashti@users.noreply.github.com>
    Co-authored-by: Shahriyar Rzayev <rzayev.sehriyar@gmail.com>

--- src/ec2instances/ec2_instance_mapping.py
+++ src/ec2instances/ec2_instance_mapping.py
@@ -8,7 +8,7 @@ from .ec2_instance_proxy import Ec2InstanceProxy, Ec2RemoteShellProxy
 
 
 class Ec2AllInstancesData:
-    def __init__(self, **kwargs: str) -> None:
+    def __init__(self) -> None:
         self._all_instances = resource("ec2").instances.all()
         self._instances_data = [
             (
